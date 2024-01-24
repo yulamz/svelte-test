7cd50745c047d06dc0294ac6477be37118c17de7
zhanglanyu <lanyu.zhang@tec-do.com>
2024-01-24 14:43:07 +0800

    feat: 初始化项目

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import IconLink from './components/IconLink.svelte';
+</script>
+
+<main>
+	<IconLink icon="🚀" link="https://pnpm.io/zh/filtering" />
+</main>
+
+<style>
+
+</style>
---
+++ svelte-app/src/components/IconLink.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let icon;
+    export let link;
+  
+    function handleClick() {
+      window.location.href = link;
+    }
+  </script>
+  
+  <main>
+    <div on:click={handleClick} class="icon">{icon}</div>
+  </main>
+  
+  <style>
+    .icon {
+      cursor: pointer;
+    }
+  </style>
+  
+  
+    
